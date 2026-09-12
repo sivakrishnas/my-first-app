31b82598090bf5e66fc83bab9a61dd1275c6f1cb
sivakrishnas <siva.krishnas473@gmail.com>
2026-09-13 01:21:57 +0530

    Remove unused RouterLink import from service list page component

--- src/app/pages/service-list/service-list-page.component.ts
+++ src/app/pages/service-list/service-list-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute} from '@angular/router';
 import { ServiceCatalogService } from '../../services/service-catalog.service';
 import { MobileNavigationComponent } from '../../components/mobile-navigation/mobile-navigation.component';
 import { SiteHeaderComponent } from '../../components/site-header/site-header.component';
@@ -7,7 +7,7 @@ import { SiteFooterComponent } from '../../components/site-footer/site-footer.co
 
 @Component({
   selector: 'app-service-list-page',
-  imports: [RouterLink, MobileNavigationComponent, SiteHeaderComponent, SiteFooterComponent],
+  imports: [MobileNavigationComponent, SiteHeaderComponent, SiteFooterComponent],
   templateUrl: './service-list-page.component.html',
   styleUrl: '../service-ui.css',
 })
